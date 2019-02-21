650b74ca0db37a62780538f43e7a9a99ccc68977
Henry Dang <henrydangprg@gmail.com>
2019-02-21 15:56:30 -0500

    Turn the MySQL results into JSON in mysql_util

--- util/mysql_util.js
+++ util/mysql_util.js
@@ -3,13 +3,14 @@ var mysql_config = require('../config/mysql_config');
 var pool = mysql.createPool(mysql_config.databaseOptions);
 
 // Wraps your query with error-handling so that you don't have to
-// keep doing if(error) throw err each time
+// keep doing if(error) throw err each time. Also turns it into a nice
+// JSON format
 function getQuery(query, callback){
     pool.query(query, function(err, results, fields) {
         if(err){
             throw "[MySQL] " + err
         } else {
-            callback(results);
+            callback(JSON.parse(JSON.stringify(results)));
         }
     });
 }
