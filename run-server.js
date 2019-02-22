9384a0ab5078b31ede5e37d0995cf1b6a6fc44bf
Henry Dang <henrydangprg@gmail.com>
2019-02-22 12:13:15 -0500

    Add new route for user IDs and add templating system

--- run-server.js
+++ run-server.js
@@ -1,11 +1,43 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+
+var mysql = require('mysql');
 var mysql_util = require('./util/mysql_util')
 
+var express_hbr = require('express-handlebars');
+
+var hbr = express_hbr.create({});
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbr.engine);
+app.set('view engine', 'handlebars');
 
 app.listen(8080, function(){
     console.log("Server started on port 8080");
 });
 
+// This is nececssary because the SQL call happens aysnchronously
+function renderUserPage(res, name){
+    res.render('main',{
+        showTitle : true,
+        helpers : {
+            user: function() { 
+                return name;
+            }
+        }
+    });
+}
+app.get('/user/:userId', function(req, res){
+    var id = parseInt(req.params.userId);
+    var query = mysql.format('SELECT username FROM users WHERE ID = ?', [id]);
+    var username = "";
+    mysql_util.getQuery(query, function(results){
+        try {
+            renderUserPage(res, results[0].username)
+        } catch (err){
+            console.log("[Rendering Error] " + err);
+            renderUserPage(res, "User Not Found");
+        }
+    });
+});
